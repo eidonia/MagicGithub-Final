6e1068029ee8d8ce0e5837a60a132da3f23c8058
bastien <bastien_rambeaud@hotmail.com>
2019-11-08 21:46:56 +0100

    Initial commit

---
+++ app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
@@ -0,0 +1,28 @@
+package com.openclassrooms.magicgithub.repository;
+
+import com.openclassrooms.magicgithub.api.ApiService;
+import com.openclassrooms.magicgithub.model.User;
+
+import java.util.List;
+
+public class UserRepository {
+
+    private final ApiService apiService;
+
+    public UserRepository(ApiService apiService) {
+        this.apiService = apiService;
+    }
+
+    public List<User> getUsers() {
+        List<User> getUser = apiService.getUsers();
+        return getUser;
+    }
+
+    public void generateRandomUser() {
+        apiService.generateRandomUser();
+    }
+
+    public void deleteUser(User user) {
+        apiService.deleteUser(user);
+    }
+}
